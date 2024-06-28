6fcf72648da9ff6d3d65f3b41c657a4f2197f607
LiyaNemirowa <liyanemirowa1379@gmail.com>
2024-06-28 21:57:58 +0600

    Добавляет правки

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,3 +1,9 @@
+const params = new URL(location.toString()).searchParams;
+const edit = !!params.get('edit');
+if (params.get('edit') !== null) {
+  document.body.contentEditable = true;
+}
+
 const pageHeaderElement = document.querySelector('.page-header');
 const hamburgerElement = pageHeaderElement.querySelector('.page-header__hamburger');
 
