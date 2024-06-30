be3b009bc6d972ef6b7541c516a49b0959179526
LiyaNemirowa <liyanemirowa1379@gmail.com>
2024-07-01 01:31:02 +0600

    Правки ко второй защите 3

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -17,3 +17,22 @@ hamburgerElement.addEventListener('click', function () {
   }
 });
 pageHeaderElement.classList.remove('page-header--no-js');
+
+window.ymaps.ready(() => {
+  const mapElement = document.querySelector('.map');
+  const pictureElement = mapElement.querySelector('picture');
+  const template = mapElement.querySelector('template').innerHTML;
+
+  const map = new window.ymaps.Map(mapElement, {
+    center: [59.9387165, 30.3230474],
+    controls: [],
+    zoom: 16
+  });
+
+  map.geoObjects.add(new window.ymaps.Placemark(map.getCenter(), null, {
+    iconLayout: window.ymaps.templateLayoutFactory.createClass(template),
+  }));
+  map.behaviors.disable('scrollZoom');
+
+  pictureElement.remove();
+});
